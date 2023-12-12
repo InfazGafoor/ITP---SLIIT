1f0f94c092bb79044ba13300f61eab64cb5a67bd
InfazGafoor <126438112+InfazGafoor@users.noreply.github.com>
2023-12-12 15:26:50 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Navbar from "./components/navbar.component"
-
-import EditCourses from "./components/Edit-Courses.component";
-import CreateCourses from "./components/Create-Courses.component";
-import CoursesList from "./components/list-Courses.component";
-import report from "./components/Report";
-
-
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-    return ( <Router >
-        <div className = "container" >
-       
-        <Navbar/ >
-        <br/ >
-        <Route path = "/" exact component = { CoursesList }/>
-        <Route path = "/edit/:id" component = { EditCourses }/> 
-        <Route path = "/create" component = { CreateCourses }/> 
-        <Route path = "/report/" component = { report }/>
-        
-        </div > 
-      
-        </Router>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
